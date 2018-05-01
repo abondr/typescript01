a6b86eabbbdb9ec404861da9b545c2e6a50c34bd
root <abondr>
2018-05-01 17:54:36 +0530

    master time ticker

---
+++ page03.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>
+            page 03
+        </title>
+    </head>
+    <body>
+    <div id='container'></div>
+        <script type="text/javascript" src="page03.js"></script>
+    </body>
+</html>
---
+++ page03.ts
@@ -0,0 +1,19 @@
+class Greeter{
+	element : HTMLElement;
+	span : HTMLElement;
+	timerToken : number;
+	constructor(element : HTMLElement){
+		this.element = element;
+		this.element.innerHTML += "The Time is => ";
+		this.span = document.createElement("span");
+		this.element.appendChild(this.span);
+		this.span.innerText = new Date().toUTCString();
+	}
+	start(){
+		this.timerToken = setInterval(()=>this.span.innerHTML = new Date().toUTCString(),500);
+	}	
+	stop(){
+		clearTimeout(this.timerToken);
+	}
+}
+new Greeter(document.getElementById('container')).start();
